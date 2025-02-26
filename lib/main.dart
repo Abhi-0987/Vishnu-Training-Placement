f1ce5121249862d053994c1c25bcceccb9210565
pranayshl <163889726+pranayshl@users.noreply.github.com>
2025-02-27 01:47:15 +0530

    Welcome screen added (#14)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:vishnu_training_and_placements/screens/student_profile.dart';
-//import 'package:vishnu_training_and_placements/screens/mark_attendance.dart';
-//import 'screens/AdminAttendanceScreen.dart';
-import 'screens/student_homescreen.dart';
+
+import 'screens/mark_attendance.dart';
 
 void main() {
   runApp(const MaterialApp(home: MainApp()));
@@ -13,9 +11,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StudentHomeScreen(),
-    );
+      home: MarkAttendancePage());
   }
 }
