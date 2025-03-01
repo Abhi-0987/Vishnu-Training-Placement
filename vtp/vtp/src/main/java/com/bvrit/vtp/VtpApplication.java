2b3b20d3cf707ec756bb82238e4440ae263a8bfc
Shashank250705 <140614365+Shashank250705@users.noreply.github.com>
2025-03-01 20:19:34 +0530

    Spring setup to project (#20)

---
+++ vtp/vtp/src/main/java/com/bvrit/vtp/VtpApplication.java
@@ -0,0 +1,13 @@
+package com.bvrit.vtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtpApplication.class, args);
+	}
+
+}
---
+++ vtp/vtp/src/test/java/com/bvrit/vtp/VtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvrit.vtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
