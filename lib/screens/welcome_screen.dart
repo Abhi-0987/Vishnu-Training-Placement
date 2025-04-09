eec3575afc6a63813674e38a119ee52abcd15df0
Abhinay Ganjai <abhinayganjai@gmail.com>
2025-04-09 23:01:06 +0530

    changed logos

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -127,161 +127,168 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             ),
           ),
 
-          Column(
-            children: [
-              SizedBox(height: height * 0.3),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: height * 0.3),
 
-              Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.center,
-                    child: SizedBox(
-                      width: width * 0.8,
-                      child: Image.asset('assets/tlogo.png'),
+                Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: SizedBox(
+                        width: width * 0.8,
+                        child: Image.asset('assets/tlogo.png'),
+                      ),
                     ),
-                  ),
-                  Column(
-                    children: [
-                      SizedBox(height: height * 0.03),
-                      Align(
-                        alignment: Alignment.center,
-                        child: ClipRRect(
-                          child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-                            child: Container(
-                              height: height * 0.32,
-                              width: width * 0.96,
-                              padding: EdgeInsets.all(width * 0.05),
-                              decoration: BoxDecoration(
-                                color: const Color.fromRGBO(255, 255, 255, 0.1),
-                                borderRadius: BorderRadius.circular(
-                                  width * 0.04,
+                    Column(
+                      children: [
+                        SizedBox(height: height * 0.03),
+                        Align(
+                          alignment: Alignment.center,
+                          child: ClipRRect(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                              child: Container(
+                                height: height * 0.32,
+                                width: width * 0.96,
+                                padding: EdgeInsets.all(width * 0.05),
+                                decoration: BoxDecoration(
+                                  color: const Color.fromRGBO(
+                                    255,
+                                    255,
+                                    255,
+                                    0.1,
+                                  ),
+                                  borderRadius: BorderRadius.circular(
+                                    width * 0.04,
+                                  ),
+                                  border: Border.all(
+                                    color: Colors.white,
+                                    width: 1,
+                                  ),
                                 ),
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 1,
-                                ),
-                              ),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  // Admin Button
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(18),
-                                      gradient: const LinearGradient(
-                                        colors: [
-                                          Colors.pinkAccent,
-                                          Colors.orangeAccent,
-                                        ],
-                                      ),
-                                    ),
-                                    padding: const EdgeInsets.all(3),
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.black,
-                                        elevation: 0,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(
-                                            18,
-                                          ),
-                                        ),
-                                        padding: const EdgeInsets.symmetric(
-                                          horizontal: 50,
-                                          vertical: 12,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    // Admin Button
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(18),
+                                        gradient: const LinearGradient(
+                                          colors: [
+                                            Colors.pinkAccent,
+                                            Colors.orangeAccent,
+                                          ],
                                         ),
                                       ),
-                                      onPressed: () {
-                                        Navigator.pushNamed(
-                                          context,
-                                          AppRoutes.adminLogin,
-                                        );
-                                      },
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Text(
-                                            'Admin',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 28,
-                                              fontFamily: 'Alata',
+                                      padding: const EdgeInsets.all(3),
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.black,
+                                          elevation: 0,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(
+                                              18,
                                             ),
                                           ),
-                                          const SizedBox(width: 15),
-                                          Image.asset(
-                                            'assets/admin.png',
-                                            height: 30,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 20),
-                                  // Student Button
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(18),
-                                      gradient: const LinearGradient(
-                                        colors: [
-                                          Colors.pinkAccent,
-                                          Colors.orangeAccent,
-                                        ],
-                                      ),
-                                    ),
-                                    padding: const EdgeInsets.all(3),
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.black,
-                                        elevation: 0,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(
-                                            18,
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 50,
+                                            vertical: 12,
                                           ),
                                         ),
-                                        padding: const EdgeInsets.symmetric(
-                                          horizontal: 50,
-                                          vertical: 12,
+                                        onPressed: () {
+                                          Navigator.pushNamed(
+                                            context,
+                                            AppRoutes.adminLogin,
+                                          );
+                                        },
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Text(
+                                              'Admin',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 28,
+                                                fontFamily: 'Alata',
+                                              ),
+                                            ),
+                                            const SizedBox(width: 15),
+                                            Image.asset(
+                                              'assets/admin.png',
+                                              height: 30,
+                                            ),
+                                          ],
                                         ),
                                       ),
-                                      onPressed: () {
-                                        Navigator.pushNamed(
-                                          context,
-                                          AppRoutes.studentLogin,
-                                        );
-                                      },
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          const Text(
-                                            'Student',
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 28,
-                                              fontFamily: 'Alata',
+                                    ),
+                                    const SizedBox(height: 20),
+                                    // Student Button
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(18),
+                                        gradient: const LinearGradient(
+                                          colors: [
+                                            Colors.pinkAccent,
+                                            Colors.orangeAccent,
+                                          ],
+                                        ),
+                                      ),
+                                      padding: const EdgeInsets.all(3),
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.black,
+                                          elevation: 0,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(
+                                              18,
                                             ),
                                           ),
-                                          const SizedBox(width: 15),
-                                          Image.asset(
-                                            'assets/student.png',
-                                            height: 30,
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 50,
+                                            vertical: 12,
                                           ),
-                                        ],
+                                        ),
+                                        onPressed: () {
+                                          Navigator.pushNamed(
+                                            context,
+                                            AppRoutes.studentLogin,
+                                          );
+                                        },
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            const Text(
+                                              'Student',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 28,
+                                                fontFamily: 'Alata',
+                                              ),
+                                            ),
+                                            const SizedBox(width: 15),
+                                            Image.asset(
+                                              'assets/student.png',
+                                              height: 30,
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
