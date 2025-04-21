c13d1e1b785e0bcaae19a41a3c2d66ebc0799217
Shashank250705 <140614365+Shashank250705@users.noreply.github.com>
2025-04-21 12:55:56 +0530

    Implemented AdminHomeScreen (#49)
    * Implemented AdminHomeScreen
    
    * implemented admin home screen with nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vishnu_training_and_placements/routes/app_routes.dart';
 import 'package:vishnu_training_and_placements/screens/event_venue.dart';
+import 'package:vishnu_training_and_placements/screens/admin_homescreen.dart';
 
 void main() {
   runApp(const MaterialApp(home: MainApp()));
@@ -13,10 +14,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //initialRoute: AppRoutes.splash,
-      //routes: AppRoutes.routes,
+      initialRoute: AppRoutes.splash,
+      routes: AppRoutes.routes,
       // home: StudentLoginScreen(isAdmin: false),
-      home: EventVenueScreen(),
+      //home: AdminHomeScreen(),
     );
   }
 }
