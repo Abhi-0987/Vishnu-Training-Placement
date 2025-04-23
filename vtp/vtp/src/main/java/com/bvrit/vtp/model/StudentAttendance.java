59c73e7d4a08fa43ce75043ebf87f06c38f52559
Akshaya Mudam <akshayamudam@gmail.com>
2025-04-23 22:36:17 +0530

    added attendance model and student details model

---
+++ vtp/vtp/src/main/java/com/bvrit/vtp/model/StudentAttendance.java
@@ -0,0 +1,29 @@
+package com.bvrit.vtp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "student_attendance")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentAttendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "student_email")
+    private String email;
+
+    @Column(nullable = false)
+    private boolean present = false;
+
+    private LocalDate date;
+
+}
