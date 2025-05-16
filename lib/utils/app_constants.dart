4f514c8d1b1108bb1b7fafbe5846e0d7ffa73352
Tanishq Sai Govind <tanishqsaigovind@gmail.com>
2025-05-16 16:59:50 +0530

    added sections

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppConstants {
   // static const String backendUrl = 'https://vtp-8ga9.onrender.com';
-  static const String backendUrl =
-      'http://localhost:8080'; //change according to your laptop's ipAddress
+    static const String backendUrl = 'http://localhost:8080';
   static const Color gradient_1 = Colors.pinkAccent;
   static const Color gradient_2 = Colors.orangeAccent;
   static const Color primaryColor = Color.fromARGB(255, 165, 7, 193);
