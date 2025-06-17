f23af9bf7c1f9e33f6078c7729c897ca8a4f73b0
Akshaya Mudam <akshayamudam@gmail.com>
2025-06-17 22:54:43 +0530

    added displaying of student schedules

--- lib/screens/student_schedule_screen.dart
+++ lib/screens/student_schedule_screen.dart
@@ -34,7 +34,7 @@ class _StudentSchedulesScreenState extends State<StudentSchedulesScreen> {
 Future<void> getUserBranch() async {
     try {
       final prefs = await SharedPreferences.getInstance();
-      final branch = prefs.getString('branch');
+      final branch = prefs.getString('studentbranch');
 
       print('Retrieved branch from SharedPreferences: $branch');
 
